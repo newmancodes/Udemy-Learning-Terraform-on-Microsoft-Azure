db6dce1bba07d4ca1089b7c04106fe8bd4d5f8b2
Stephen Newman <steve@newman.digital>
2022-03-21 12:04:59 +0000

    Module 3 Complete - Added terraform.tfvars for demo purposes and nothing secret in there.

--- main.tf
+++ main.tf
@@ -12,6 +12,31 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "web_server_rg" {
-    name = "web-rg"
-    location = "UK South"
+    name     = var.web_server_rg
+    location = var.web_server_location
+}
+
+resource "azurerm_virtual_network" "web_server_vnet" {
+    name                = "${var.resource_prefix}-vnet"
+    location            = var.web_server_location
+    resource_group_name = azurerm_resource_group.web_server_rg.name
+    address_space       = [var.web_server_address_space]
+}
+
+resource "azurerm_subnet" "web_server_subnet" {
+    name                    = "${var.resource_prefix}-subnet"
+    resource_group_name     = azurerm_resource_group.web_server_rg.name
+    virtual_network_name    = azurerm_virtual_network.web_server_vnet.name
+    address_prefixes        = [var.web_server_address_prefix]
+}
+
+resource "azurerm_network_interface" "web_server_nic" {
+    name                    = "${var.web_server_name}-nic"
+    location                = var.web_server_location
+    resource_group_name     = azurerm_resource_group.web_server_rg.name
+    ip_configuration {
+        name                            = "${var.web_server_name}-ip"
+        subnet_id                       = azurerm_subnet.web_server_subnet.id
+        private_ip_address_allocation   = "dynamic"
+    }
 }
